fdecc68d74edfa976a3c9313729eea30a239577e
Jihadul Islam <109913091+jihad-dev@users.noreply.github.com>
2025-08-24 15:21:26 +0600

    Update auth.services.ts

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -24,7 +24,7 @@ export const loginUser = async (email: string, password: string) => {
       role: user?.role,
     },
     config.jwt_secret as string,
-    { expiresIn: "20s" }
+    { expiresIn: "3d" }
   );
   // refresh token
   const refreshToken = jwt.sign(
@@ -34,7 +34,7 @@ export const loginUser = async (email: string, password: string) => {
       role: user?.role,
     },
     config.jwt_refresh_secret as string,
-    { expiresIn: "20s" }
+    { expiresIn: "7d" }
   );
   return {
     accessToken,
